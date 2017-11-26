4928161651a866c17a9e8979432ec7c8dfa69da5
DaMatrix <joey.rabil@gmail.com>
2017-11-26 14:19:39 +0100

    read config

--- src/main/java/net/daporkchop/bettermaps/Block.java
+++ src/main/java/net/daporkchop/bettermaps/Block.java
@@ -5,11 +5,13 @@ public class Block {
     public final int blockMeta;
     public final MapColor color;
     public final String registryName;
+    public final String displayName;
 
-    public Block(int id, int meta, MapColor color, String registryName) {
+    public Block(int id, int meta, MapColor color, String registryName, String displayName) {
         this.blockId = id;
         this.blockMeta = meta;
         this.color = color;
         this.registryName = registryName;
+        this.displayName = displayName;
     }
 }
--- src/main/java/net/daporkchop/bettermaps/Generator.java
+++ src/main/java/net/daporkchop/bettermaps/Generator.java
@@ -1,22 +1,63 @@
 package net.daporkchop.bettermaps;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Hashtable;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Generator {
     public static final Set<Block> blocks = new LinkedHashSet<Block>();
+    public static final Map<Integer, MapColor> colors = new Hashtable<Integer, MapColor>();
 
     public static final void readBlocksFromConfig() {
         try {
+            System.out.println("Loading block data...");
             blocks.clear();
+            colors.clear();
             String config = IOUtils.toString(Generator.class.getResourceAsStream("/colors.json"), Charset.defaultCharset());
-            System.out.println(config);
+            JsonObject object = new JsonParser().parse(config).getAsJsonObject();
+
+            {
+                JsonObject colors = object.getAsJsonObject("colors");
+                for (Map.Entry<String, JsonElement> entry : colors.entrySet()) {
+                    JsonObject jColor = entry.getValue().getAsJsonObject();
+                    int index = Integer.parseInt(entry.getKey());
+                    int r = jColor.getAsJsonPrimitive("red").getAsInt();
+                    int g = jColor.getAsJsonPrimitive("green").getAsInt();
+                    int b = jColor.getAsJsonPrimitive("blue").getAsInt();
+                    int a = jColor.getAsJsonPrimitive("alpha").getAsInt();
+                    MapColor color = new MapColor(r, g, b, a, index);
+                    Generator.colors.put(index, color);
+                }
+            }
+            {
+                JsonArray blocks = object.getAsJsonArray("blocks");
+                for (JsonElement element : blocks) {
+                    JsonObject jBlock = element.getAsJsonObject();
+                    String registryName = jBlock.getAsJsonPrimitive("registryName").getAsString();
+                    String localizedName = jBlock.getAsJsonPrimitive("localizedName").getAsString();
+                    int id = jBlock.getAsJsonPrimitive("id").getAsInt();
+                    int meta = jBlock.getAsJsonPrimitive("meta").getAsInt();
+                    MapColor color = colors.get(Integer.parseInt(jBlock.getAsJsonPrimitive("color").getAsString()));
+                    if (color == null) {
+                        throw new IllegalStateException("Invalid map color: " + jBlock.getAsJsonPrimitive("color").getAsString());
+                    }
+                    Generator.blocks.add(new Block(id, meta, color, registryName, localizedName));
+                }
+            }
+            System.out.println("Loaded!");
         } catch (IOException e) {
             throw new IllegalStateException(e); //this shouldn't ever be possible
         }
+
+        return;
     }
 }
